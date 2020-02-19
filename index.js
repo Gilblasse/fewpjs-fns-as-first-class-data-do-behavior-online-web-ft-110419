eb2581d16b4bcdae0eec4045578e1c28ed0fdf2d
Nethelbert <gilblasse@gmail.com>
2020-02-19 06:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(stringTime) {
   num = parseInt(stringTime.split(":")[0]);
+  num < 12 ?   :
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(stringTime) {
